c223e5db445ac41c6e987f56ea1cccaf716c5760
AMit <amitkulhari26@gmail.com>
2020-07-15 00:42:25 +0530

    Bootcamp model added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Load env variables
@@ -22,12 +23,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-    console.log(`Server is running in ${process.env.NODE_ENV} on ${PORT}`);
+    console.log(`Server is running in ${process.env.NODE_ENV} on ${PORT}`.yellow.bold);
 });
 
 // Handle unhandled promise rejection
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red.bold);
     server.close(() => {
         process.exit(1);
     });
