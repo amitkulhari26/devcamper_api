9f64f40f185d2020298b8b4a4e5b1f88701c1459
AMit <amitkulhari26@gmail.com>
2020-07-15 00:01:23 +0530

    connected to database

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
+// Load env variables
 dotenv.config({ path: './config/config.env' });
-
+// Connect to db
+connectDB();
+// Route Files
 const bootcamps = require('./routes/bootcams');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcamps);
 
