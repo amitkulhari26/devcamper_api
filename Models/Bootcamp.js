c82986c7e15be0184f2f7baa478cc2294f1a8ae5
AMit <amitkulhari26@gmail.com>
2020-07-16 23:49:45 +0530

    cascade delete feature added

--- Models/Bootcamp.js
+++ Models/Bootcamp.js
@@ -126,6 +126,12 @@ BootcampSchema.pre('save', async function (next) {
     next();
 });
 
+// cascade delete courses when bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+});
+
 // Reverse Populate with virtuals
 BootcampSchema.virtual('courses', {
     ref: 'Course',
